c96d693bd2bad1bbf4d5da8fca51027c76fa4127
PatrykPytel <127932899+PatrykPytel@users.noreply.github.com>
2023-05-08 16:35:09 +0200

    xc

--- gfc/Assets/Scripts/Healthmanager.cs
+++ gfc/Assets/Scripts/Healthmanager.cs
@@ -5,10 +5,19 @@ using UnityEngine.UI;
 
 public class Healthmanager : MonoBehaviour
 {
+    void Start()
+    {
+        
+    }
     public int health;
     public Image[] hearts;
     public Sprite pelneserce;
     public Sprite pusteserce;
+    public bool enemyIsClose;
+    public GameObject Huj1;
+    public GameObject Huj2;
+    
+
 
     void Update()
     {
@@ -20,6 +29,30 @@ public class Healthmanager : MonoBehaviour
         {
             hearts[i].sprite = pelneserce;
         }
+        if (enemyIsClose)
+        {
+            Debug.Log(health);
+            health-=2;
+            Physics2D.IgnoreCollision(Huj1.GetComponent<Collider2D>(), Huj2.GetComponent<Collider2D>());
+            
+
+            
+            
+            
+        }
+
+         
 
     }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+
+            enemyIsClose = true;
+            
+        }
+    }   
+
+    
 }
