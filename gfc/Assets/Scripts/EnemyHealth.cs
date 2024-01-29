b32cddc8eb78a9ae1af6341ece4f28f7ca1c69ab
PatrykPytel <127932899+PatrykPytel@users.noreply.github.com>
2024-01-29 21:44:26 +0100

    death animation
    ds

--- gfc/Assets/Scripts/EnemyHealth.cs
+++ gfc/Assets/Scripts/EnemyHealth.cs
@@ -9,11 +9,14 @@ public class EnemyHealth : MonoBehaviour
     public Animator animator;
     private float previoushp;
     private bool die;
+    public float dyinganim;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     // Update is called once per frame
     void Start() 
     {
         previoushp = mhp;
+        rb= GetComponent<Rigidbody2D>();
     }
     public void monsterhurt()
     {
@@ -23,7 +26,8 @@ public class EnemyHealth : MonoBehaviour
     {
         if(mhp<=0) {
             animator.SetBool("isdead", true);
-            Destroy(Monster);
+            rb.simulated = false;
+            Invoke("monsterdead", dyinganim);
         }
         else if(mhp<previoushp) {
             animator.SetBool("strucked", true);
@@ -33,6 +37,9 @@ public class EnemyHealth : MonoBehaviour
             animator.SetBool("strucked", false);
         }
 
+    }
+    void monsterdead() {
+        Destroy(Monster);
     }     
 }
 //        if (health<=0) 
