bba8f02a1f27312f775b1c57ded72c822533956d
PatrykPytel <127932899+PatrykPytel@users.noreply.github.com>
2024-02-06 00:14:36 +0100

    combos animation

--- gfc/Assets/basicattack.cs
+++ gfc/Assets/basicattack.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class basicattack : MonoBehaviour
 {
     public Animator animator;
+    private float comboTime =1f;
+	private float comboCounter;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +16,17 @@ public class basicattack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse0)) {
+        if(Input.GetKeyDown(KeyCode.Mouse0) && comboCounter>0f) {
+            animator.SetBool("2attack", true);
+            comboCounter = -1f;
+        } else if(Input.GetKeyDown(KeyCode.Mouse0)) { 
             animator.SetBool("isattacking", true);
-
-        }else { 
+            comboCounter = comboTime;
+        }
+        else { 
+            comboCounter-=Time.deltaTime;
             animator.SetBool("isattacking", false);
+            animator.SetBool("2attack",false);
         }
         
     }
