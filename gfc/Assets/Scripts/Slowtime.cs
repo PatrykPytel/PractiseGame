e7359d2e30cdb9a26a3e204d8159c431df7b258f
PatrykPytel <127932899+PatrykPytel@users.noreply.github.com>
2024-03-11 22:30:14 +0100

    y5rt6h

--- gfc/Assets/Scripts/Slowtime.cs
+++ gfc/Assets/Scripts/Slowtime.cs
@@ -11,24 +11,26 @@ public class Slowtime : MonoBehaviour
     Finalmovement move;
     Rigidbody2D rb;
     private bool timeslowed = false;
+    private SpriteRenderer Background;
     // Start is called before the first frame update
     void Start()
     {
         move= GameObject.Find("Player").GetComponent<Finalmovement>();
+        Background = GameObject.Find("Background").GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         cooldown -= Time.deltaTime; 
-        if(cooldown <= 0 && timeslowed)
+        if(cooldown <= 0 && timeslowed && move.m_Grounded)
         {
             Time.timeScale = 1f;
             move.runSpeed = move.startspeed;
             move.m_JumpForce = move.startjumpforce;
             move.rb.gravityScale = 3f;
             timeslowed = false ;
-
+            Background.color = Color.white;
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -42,6 +44,7 @@ public class Slowtime : MonoBehaviour
             move.rb.gravityScale = 10f;
             move.m_JumpForce = move.slowedjumpforce;
             timeslowed = true;
+            Background.color = new Color(0,0,1,0.5f);
         }
     }
 }
