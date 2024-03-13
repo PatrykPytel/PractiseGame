c237d6fa6680d4317221a0acd5403e4120861590
PatrykPytel <127932899+PatrykPytel@users.noreply.github.com>
2024-03-14 00:28:10 +0100

    shooting

--- gfc/Assets/Scripts/Wand.cs
+++ gfc/Assets/Scripts/Wand.cs
@@ -14,6 +14,7 @@ public class Wand : MonoBehaviour
     void Update()
     {
         Vector3 mousePosition = Input.mousePosition;
+
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
         transform.up = direction;
